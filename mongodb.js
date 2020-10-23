4301ccb2ae676382216cf6e7ce9b6d3038d012a8
giovanizanetti <zanetti.giovani@gmail.com>
2020-10-23 18:41:07 +0200

    Create project.

---
+++ mongodb.js
@@ -0,0 +1,13 @@
+const mongodb = require("mongodb");
+const mongoClient = mongodb.mongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const db = "task-manager";
+
+mongodb.MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) return console.log("Unable to connect to database");
+  }
+);
