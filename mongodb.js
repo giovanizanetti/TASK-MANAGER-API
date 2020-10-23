a0f28cc9b3fc248534ed6a78066125dfde979e4d
giovanizanetti <zanetti.giovani@gmail.com>
2020-10-23 19:24:17 +0200

    Add user to db.

--- mongodb.js
+++ mongodb.js
@@ -2,13 +2,19 @@ const mongodb = require("mongodb");
 const mongoClient = mongodb.mongoClient;
 
 const connectionURL = "mongodb://127.0.0.1:27017";
-const db = "task-manager";
+const dbName = "task-manager";
 
 mongodb.MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (error, client) => {
     if (error) return console.log("Unable to connect to database");
-    console.log("Connected correctly");
+
+    const db = client.db(dbName);
+
+    db.collection("users").insertOne({
+      Name: "Lindinha",
+      age: 33,
+    });
   }
 );
