99d414fe92c242dc872abd453f61b4d368fb7866
giovanizanetti <zanetti.giovani@gmail.com>
2020-10-27 18:14:39 +0100

    Change user and task status code to 201.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,18 +5,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-
-// const Task = mongoose.model("task", {
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-// });
-// const task = new Task({
-//   description: "Go to the gin  ",
-// });
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((err) => {
       res.status(400).send(err);
@@ -25,7 +25,7 @@ app.post("/tasks", (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((err) => {
       res.status(400).send(err);
