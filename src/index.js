7d3ad0de4bd53fdc9a78b600c342f44fb7565a91
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-08 17:43:40 +0100

    Remove avatar from the profile response to improve performance.

--- src/index.js
+++ src/index.js
@@ -5,22 +5,6 @@ require("./db/mongoose.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require("multer");
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1000000, //1MB
-  },
-  fileFilter(req, file, cb) {
-    // Match word files
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error("File must be a Word document"));
-    }
-
-    cb(undefined, true);
-  },
-});
-
 app.post("/upload", upload.single("upload"), (req, res) => {
   console.log("requesteddddddd");
   res.send();
