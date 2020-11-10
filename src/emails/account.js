df89a2a62b73373b4fb12f72680d239683474885
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-10 18:35:52 +0100

    Create cancelation email

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,5 +1,6 @@
 const SEND_GRID_API_KEY =
   "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";
+const SENDER_EMAIL = "zanetti.giovani@gmail.com";
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(SEND_GRID_API_KEY);
@@ -14,12 +15,25 @@ sgMail.setApiKey(SEND_GRID_API_KEY);
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: "zanetti.giovani@gmail.com",
+    from: SENDER_EMAIL,
     subject: "Welcome!!",
-    text: `Hi ${name}! \n Welcome to the app.`,
+    text: `Hi ${name}!\n
+          Welcome to the app.`,
+  });
+};
+
+const cancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: SENDER_EMAIL,
+    subject: "Good Bye",
+    text: `Dear ${name}.\n
+          We are sorry to se you go. You were a valuable costumer for us.\n 
+          If you change your mind in the future we are going to be always here.`,
   });
 };
 
 module.exports = {
   sendWelcomeEmail,
+  cancelationEmail,
 };
