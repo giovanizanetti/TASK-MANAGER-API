0a018bf0620758b80a59158ab77467c68b78f8b6
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-10 18:07:48 +0100

    Create welcome email meassage.

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,3 +1,25 @@
 const SEND_GRID_API_KEY =
   "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";
 const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(SEND_GRID_API_KEY);
+
+// sgMail.send({
+//   to: "zanetti.giovani@gmail.com",
+//   from: "zanetti.giovani@gmail.com",
+//   subject: "Testting first email",
+//   text: "I hope it worked.",
+// });
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "zanetti.giovani@gmail.com",
+    subject: "Welcome!!",
+    text: `Hi ${name}! \n Welcome to the app.`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+};
