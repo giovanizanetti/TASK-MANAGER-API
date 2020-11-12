280aca65da83557e46d16d912bdfce1f14a951f4
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-12 20:11:19 +0100

    Create app.js to to work with supertest.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-const express = require("express");
-const router = require("./routers");
-require("./db/mongoose.js");
-
-const app = express();
+const app = require("./app");
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
