6403b20aab4bb7bfa5cbb338d443563ccef95dc1
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-13 09:19:35 +0100

    Create test 'should get profile for user' test.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,23 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
+const { TOKEN_SECRET } = process.env;
+
+const existentUserId = new mongoose.Types.ObjectId();
 
 const existentUser = {
+  _id: existentUserId,
   name: "Alex",
   email: "alex@test.com",
   age: 33,
   password: "AlexTest123?",
+  tokens: [
+    {
+      token: jwt.sign({ _id: existentUserId }, TOKEN_SECRET),
+    },
+  ],
 };
 
 const nonexistentUser = {
@@ -14,11 +25,14 @@ const nonexistentUser = {
   email: "anne@test.com",
 };
 
+// Clear created user by sigup unit test
+// Create a default user for authenticated unit tests
 beforeEach(async () => {
   await User.deleteMany();
   await new User(existentUser).save();
 });
 
+// TESTS
 test("Should signup a new user", async () => {
   await request(app)
     .post("/users/signup")
@@ -39,4 +53,12 @@ test("Should login existing user", async () => {
 test("Should not login nonexistent user", async () => {
   const { email, password } = nonexistentUser;
   await request(app).post("/users/login").send({ email, password }).expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${existentUser.tokens[0].token}`)
+    .send()
+    .expect(200);
 });
